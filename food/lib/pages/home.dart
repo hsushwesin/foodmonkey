a8dea2bb5a01cc8c32f02c24b708ba965591c0a0
hsushwesin <hsushwesinit@gmail.com>
2024-01-27 00:05:19 +0630

    Home Title Bar

--- food/lib/pages/home.dart
+++ food/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food/utils/constants.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,7 +12,22 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        _buildTitleTaxBar("Tags"),
+        _buildTitleTaxBar("Categories"),
+      ]),
     );
   }
+}
+
+Widget _buildTitleTaxBar(text) {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+    decoration: BoxDecoration(
+        color: secondary,
+        borderRadius: BorderRadius.only(topRight: Radius.circular(80))),
+    child: Text(text,
+        style: TextStyle(
+            fontSize: 35, fontWeight: FontWeight.bold, color: normal)),
+  );
 }
